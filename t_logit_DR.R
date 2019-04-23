65ca66cc681bad16417353d384bd7142030355c4
Daniel Jacob <danielirtg@Daniel-Jacob-Moby.local>
2019-04-23 18:10:20 +0200

    some changes - now it works as expected.

--- t_logit_DR.R
+++ t_logit_DR.R
@@ -12,14 +12,14 @@ T_Logit_DR <- function(X, y, W, prop_score=NULL){
   y0 <- glm(y~., family=binomial(link='logit'),
                 data = data[W==0,])
   
-  y0_hat <- predict(y0,X)
+  y0_hat <- predict(y0,X, type="response")
   
   y1 <- glm(y~., family=binomial(link='logit'),
                 data = data[W==1,])
   
-  y1_hat <- predict(y1,X)
+  y1_hat <- predict(y1,X,type="response")
   
-  y_mo <- y1_hat - y0_hat + (W*(y-y1_hat))/prop_score - ((1-W)*(y-y0_hat)/(1-prop_score))
+  y_mo <- (y1_hat - y0_hat) + (W*(y-y1_hat))/prop_score - ((1-W)*(y-y0_hat)/(1-prop_score))
   
   t_logit_dr <- glm(y_mo~.,data=data)
     
